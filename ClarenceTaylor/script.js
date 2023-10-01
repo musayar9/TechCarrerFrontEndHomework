a1531da753560ed76d2019c32c72e4f84ca46795
Musa SAYAR <musasayar67@gmail.com>
2023-10-01 23:48:53 +0300

    finish project

--- ClarenceTaylor/script.js
+++ ClarenceTaylor/script.js
@@ -1,24 +1,38 @@
-$(document).ready(function () {
-  $(".btn").click(function () {
-    $("li").toggleClass("show");
-    $("ul").toggleClass("nav-active");
-    $(".btn").toggleClass("active");
-    $("main").toggleClass("main-active");
+
+const btn = document.querySelector(".btn")
+const li = document.querySelectorAll("li");
+console.log(li);
+const main = document.querySelector("main")
+
+btn.addEventListener("click", ()=>{
+ li.forEach((item) => {
+   item.classList.toggle("show");
+ });
+btn.classList.toggle("active")
+main.classList.toggle("main-active")
+
+})
+
+let value="";
+const items = document.querySelectorAll("a.items")
+
+const scrollToSection = (event) => {
+  event.preventDefault();
+  removeActive();
+  const targetId = event.currentTarget.getAttribute("href").substring(1);
+  const targetSection = document.getElementById(targetId);
+  if (targetSection) {
+    event.currentTarget.classList.add("active");
+    targetSection.scrollIntoView({ behavior: "smooth" });
+  }
+};
+
+const removeActive = () => {
+  items.forEach((item) => {
+    item.classList.remove("active");
   });
-   $("a[href^='#']").on("click", function (e) {
-     e.preventDefault();
-     var target = this.hash;
+};
 
-     $("html, body").animate(
-       {
-         scrollTop: $(target).offset().top,
-       },
-       10,
-       "swing",
-       function () {
-         window.location.hash = target; // Tarayıcı URL'sini güncelle
-       }
-     );
-   });
-
+items.forEach((item) => {
+  item.addEventListener("click", scrollToSection);
 });
